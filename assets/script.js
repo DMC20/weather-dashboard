b0b20ad8f747bc1fd1231858280e6fbbfb757d1f
Daniel Carazo <danielcarazo20@gmail.com>
2021-07-02 22:55:23 -0400

    completed current day display logic

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,24 @@
-
-
 var searchBtn = document.getElementById('searchBtn');
 var currentDiv = document.getElementById("current")
 
 // openweather api key
 var apiKey = 'e79c76975ad2637930a749ca25f1b0f0';
 
-
 var getCurrentWeather = function() {
-
 
     //grab value from input
     var input = document.getElementById("searchCity").value;
-   console.log(input)
+//    console.log(input)
     //create url
     //make the api call (fetch)
+    // var lat = geolocationCoordinatesinstance.latitude;
+    // var long = geolocationCoordinatesinstance.longitude;
+    // console.log(lat, long);
+
+
     var url = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + apiKey + "&units=imperial";
+    var uvIndex = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + 40.730610 + "&lon=" + -73.935242 + "&appid=" + apiKey);
+
     fetch(url)
     .then(
         (response ) => {
@@ -32,16 +35,39 @@ var getCurrentWeather = function() {
             title.textContent = input;
 
             var temp = document.createElement("p");
-            temp.textContent = "Temperature: " + data.main.temp;
+            temp.textContent = "Temperature: " + data.main.temp + " F";
 
-            card.append(title, temp);
+            var wind = document.createElement("p");
+            wind.textContent = "Wind: " + data.wind.speed + " MPH";
+            // console.log(wind);
+
+            var humidity = document.createElement("p");
+            humidity.textContent = "Humidity: " + data.main.humidity + " %";
+
+            card.append(title, temp, wind, humidity);
+            currentDiv.append(card);
+        });
+    // create elements (parent: card , child elements: wind, temp, humid, uv)
+
+    fetch(uvIndex)
+    .then((response) => {
+        return response.json()}
+        ).then((data) => {
+            console.log(data);
+
+            var card = document.createElement("div");
+            card.classList.add("currentWeather");
+
+            var index = document.createElement("p");
+            index.textContent = "UV Index: " + data.current.uvi;
+
+            card.append(index);
             currentDiv.append(card);
 
 
-        });
-    //console data
-    // create elements (parent: card , child elements: wind, temp, humid, uv)
 
+            
+        });
 }
 
 
